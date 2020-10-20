141671c1c98649c6302e23b49e0342cd8c7df414
Vinicius Emmanuel <vinicius.teixeira.m@gmail.com>
2020-10-20 03:58:21 -0300

    service de hash para senhas

---
+++ src/main/java/br/com/viniciustestezup/nossobancodigital/compartilhado/services/HashPasswordService.java
@@ -0,0 +1,23 @@
+package br.com.viniciustestezup.nossobancodigital.compartilhado.services;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HashPasswordService {
+    private static final int iterations = 8;
+    private static final int memory = 65536;
+    private static final int parallelism = 1;
+
+    public static String hash(String password) {
+        Argon2 argon2 = Argon2Factory.create();
+        return argon2.hash(iterations, memory, parallelism, password.getBytes());
+    }
+
+    public static Boolean comparaHashPassword(String passwordSemHash, String passwordHash) {
+        Argon2 argon2 = Argon2Factory.create();
+        return argon2.verify(passwordHash, passwordSemHash.getBytes());
+    }
+}
